fd2307c65c3bdb8e33efe4920c041ce70ae8a018
Lee JiHun <jihyun9114@naver.com>
2016-01-11 11:25:15 +0900

    a

--- app/src/main/java/com/example/tacademy/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/tacademy/helloandroid/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.tacademy.helloandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.btn_toast);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = getResources().getString(R.string.btn_toast);
+
+                Toast.makeText(MainActivity.this,str,Toast.LENGTH_SHORT ).show();
+            }
+        });
+
+        btn = (Button)findViewById(R.id.btn_google);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+                startActivity(intent);
+            }
+        });
+
+        btn = (Button)findViewById(R.id.btn_dial);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("tel:01026724411"));
+                startActivity(intent);
+            }
+        });
+
+        btn = (Button)findViewById(R.id.btn_other);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, OtherActivity.class );
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/tacademy/helloandroid/OtherActivity.java
@@ -0,0 +1,32 @@
+package com.example.tacademy.helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class OtherActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other);
+
+        Button btn = (Button)findViewById(R.id.btn_finish);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        Toast.makeText(this, "backPressed",Toast.LENGTH_SHORT).show();
+    }
+}
